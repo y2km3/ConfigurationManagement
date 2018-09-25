cca4fa003c6315b2d2e4d0dde86b272a41b07b68
y2km3 <yousif.kashat@gmail.com>
2018-09-25 18:45:34 -0400

    Changes to Implementaiton of Function
    Removal of public variable  (int bottles), and changes to the naming convention used in for loop. No longer is dependent to a variable, simiply iterates at 99 going down.

--- BeerSong.java
+++ BeerSong.java
@@ -1,18 +1,17 @@
 public class BeerSong {
 
-    int bottles;
-
     public static void main (String [] args) {
-        
+        BeerSong drinker = new BeerSong();
+        drinker.Ninety_Nine_Bottle_of_Beer();
     }
 
+
     public void Ninety_Nine_Bottle_of_Beer() {
-
-        bottles = 99;
-        for (bottles; 2 < bottles; bottles--);
-            System.out.println(bottles + " bottles of beer on the wall, " + bottles " bottles of beer");
-            System.out.println("take on down, pass it around, " + (bottles - 1) + " bottles of beer");
-
+        
+        for (int i = 99; 2 < i; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer");
+            System.out.println("take on down, pass it around, " + (i - 1) + " bottles of beer");
+        }
 
     }
 
