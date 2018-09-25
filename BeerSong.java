3a520f2b742aee0febdb0bbf0b9d9234b7f8fc5b
y2km3 <yousif.kashat@gmail.com>
2018-09-25 18:49:37 -0400

    Changes to Loop as well as print
    Changed the loop bound from 2 to 1, as well as printed the last two statements when the loop is done for the last beer, and no more beers on the wall.

--- BeerSong.java
+++ BeerSong.java
@@ -7,11 +7,14 @@ public class BeerSong {
 
 
     public void Ninety_Nine_Bottle_of_Beer() {
-        
-        for (int i = 99; 2 < i; i--) {
+
+        for (int i = 99; 1 < i; i--) {
             System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer");
             System.out.println("take on down, pass it around, " + (i - 1) + " bottles of beer");
         }
+
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer");
+        System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
 
     }
 
