e5f6c436c6942b76d900fc55247c858dab55a3ec
y2km3 <yousif.kashat@gmail.com>
2018-09-25 18:37:03 -0400

    Function Updated
    Implementaiton of Ninent_Nine_Bottle_of_Beer has been implimented, has not yet been tested.

---
+++ BeerSong.java
@@ -0,0 +1,21 @@
+public class BeerSong {
+
+    int bottles;
+
+    public static void main (String [] args) {
+        
+    }
+
+    public void Ninety_Nine_Bottle_of_Beer() {
+
+        bottles = 99;
+        for (bottles; 2 < bottles; bottles--);
+            System.out.println(bottles + " bottles of beer on the wall, " + bottles " bottles of beer");
+            System.out.println("take on down, pass it around, " + (bottles - 1) + " bottles of beer");
+
+
+    }
+
+
+
+}
